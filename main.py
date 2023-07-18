490e3a0f0531e1d05a05e564390bac423216cda8
VEEB Projects <75116070+veebch@users.noreply.github.com>
2023-07-18 19:50:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-# IR component based on code by Peter Hinch
-# Copyright Peter Hinch 2020-2022 Released under the MIT licence
+"""
+  main.py - a script for running a remote control turntable
+     Copyright (C) 2023 Veeb Projects https://veeb.ch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+    Infra Red component uses code by Peter Hinch
+    Copyright Peter Hinch 2020-2022 Released under the MIT licence
+    
+"""
 
 import time
 import math
