e5f5a7a0635b56dda2e4f12896793300808f84a3
VEEB Projects <75116070+veebch@users.noreply.github.com>
2023-07-18 10:22:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,78 +8,77 @@ import machine
 from ir_rx.print_error import print_error  # Optional print of error codes
 from ir_rx.nec import NEC_16
 
-A_1A_pin = 13                       # Motor drive module
-A_1B_pin = 14                       # Motor drive module (any of the pins can be driven by PWM)
-A_1A = machine.PWM(Pin(A_1A_pin))
-A_1B = machine.PWM(Pin(A_1B_pin))
-  
-# Remote control stuff
-# If you're using a different one (you probably are) use the test.py file in the ir_rx directory to get the values for your remote
+# Motor drive module
+A_1A_pin = 14
+A_1B_pin = 15
+A_1A = machine.Pin(A_1A_pin, machine.Pin.OUT)
+A_1B = machine.Pin(A_1B_pin, machine.Pin.OUT)
+A_1A.value(0) 
+A_1B.value(0) 
 
-# Set Power Pin
-pin = machine.Pin(0, machine.Pin.OUT)
-pin.value(1)
+# Remote control buttons
+BUTTONS = {
+    0x04: "speed down",
+    0x05: "stop",
+    0x06: "speed up",
+    0x1a: "hold ccw",
+    0x1b: "hold cw",
+    0x12: "reset"
+}
 
-p = Pin(16, Pin.IN)
-buttons = {
-    0x04:"speed down",
-    0x05:"stop",
-    0x06:"speed up",
-    0x1a:"hold ccw",
-    0x1b:"hold cw",
-    0x12:"reset"}
-
-def doaspin(direction):
-    global speed    # Globals...deep shame
-    global moving
-    if direction=='speed up':
-        if moving==0:
-            # If the motor isn't moving, the speed buttons do micro adjustmnets
-            # m.MotorRun('MA','forward',100,.1)
+def doaspin(direction, speed, moving):
+    if direction == 'speed up':
+        if moving == 0:
+            # If the motor isn't moving, the speed buttons do micro adjustments
+            pass
+            # m.MotorRun('MA', 'forward', 100, .1)
         else:
-            speed = speed+5
-            speed = min (100, speed)
-    elif direction=='speed down':
-        if moving==0:
-            # m.MotorRun('MA','backward',100,.1)
+            speed += 5
+            speed = min(100, speed)
+    elif direction == 'speed down':
+        if moving == 0:
+            # m.MotorRun('MA', 'backward', 100, .1)
+            pass
         else:
-            speed = speed-5
+            speed -= 5
             speed = max(0, speed)
-    elif direction=='hold cw':
+    elif direction == 'hold cw':
         if moving == -1:
-           m.MotorStop('MA',speed) 
-        if moving !=1:
-            print("motor A CW, speed ",speed,"%")
+           m.MotorStop('MA', speed) 
+        if moving != 1:
+            print("motor A CW, speed ", speed, "%")
             # m.MotorHold('MA', 'forward', speed)
+            pass
             moving = 1
-    elif direction=='hold ccw':
+    elif direction == 'hold ccw':
         if moving == 1:
-           # m.MotorStop('MA',speed) 
-        if moving !=-1:
-            print("motor A CCW, speed ",speed,"%")
+           # m.MotorStop('MA', speed)
+           pass
+        if moving != -1:
+            print("motor A CCW, speed ", speed, "%")
             # m.MotorHold('MA', 'backward', speed)
+            pass
             moving = -1
     elif direction == 'stop':
         if moving != 0:
-            # m.MotorStop('MA',speed)
+            # m.MotorStop('MA', speed)
+            pass
             moving = 0
     elif direction == 'reset':
         machine.reset()
-    return 
+    
+    return speed, moving
 
 # User callback
 def cb(data, addr, ctrl):
-    global previous   
-    if data == -1  :  # NEC protocol sends repeat codes.
+    if data == -1:  # NEC protocol sends repeat codes.
         pass
     else:
-        previous = buttons[data]
-    _thread.start_new_thread(doaspin,(previous,))
-    time.sleep(.1)
+        direction = BUTTONS[data]
+        _thread.start_new_thread(doaspin, (direction, speed, moving))
+        time.sleep(.1)
         
 def mainloop():
-    speed=100
-    moving=0
     previous = "untouched" 
     ir = NEC_16(p, cb)  # Instantiate receiver
     ir.error_function(print_error)  # Show debug information
@@ -91,4 +90,8 @@ def mainloop():
         
 
 if __name__ == '__main__':
+    speed = 100
+    moving = 0
+    p = machine.Pin(16, machine.Pin.IN)
     mainloop()
+
