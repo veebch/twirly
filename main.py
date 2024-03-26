7314624298b31d191c124d102767fdc8eb11ea1a
veebch <mart@veeb.ch>
2024-03-26 13:57:17 +0100

    access point

--- main.py
+++ main.py
@@ -1,53 +1,173 @@
-""" Test of stepper
-
- Note: Maximum step frequency is limited by the stepper motor physics.
-       In microstepping-mode high(er) step frequencies may work fine!
-"""
-
-import sys
+from phew import access_point, connect_to_wifi, is_connected_to_wifi, dns, server
+from phew.template import render_template
+import json
+import machine
+import os
 import utime
+import _thread
 import drv8825_setup
 
-
-def run_test(mot, enc, button):
-    def action(revs, mode, speed):
-        input(f"Press <Enter> to continue {revs=} {mode=} {speed=} ")
-        mot.revolutions(revs, mode, speed)
-
-        return abs(revs * mot.steps_per_revolution - mot.progress)
-
-    try:
-        # full step variants, different speeds and directions
-        # <number of steps>, <step type>, <step frequency>
-        action(1, "Full", 50)
-        action(-1, "Full", 100)
-        action(5, "Full", 400)
-        action(-5, "Full", 600)
-        # microstepping variants
-        action(1, "Half", 500)
-        action(-1, "1/4", 1000)
-        action(5, "1/8", 2000)
-        action(-5, "1/16", 4000)
-        action(3, "1/32", 8000)
-    except KeyboardInterrupt:
-        print("Interrupted from Keyboard")
-
-    mot.disable()
+AP_NAME = "pi pico"
+AP_DOMAIN = "pipico.net"
+AP_TEMPLATE_PATH = "ap_templates"
+APP_TEMPLATE_PATH = "app_templates"
+WIFI_FILE = "wifi.json"
+WIFI_MAX_ATTEMPTS = 3
 
 
-# ===================MAIN===============================
+def machine_reset():
+    utime.sleep(1)
+    print("Resetting...")
+    machine.reset()
 
-print("TEST START")
-button = None
+
+def setup_mode():
+    print("Entering setup mode...")
+
+    def ap_index(request):
+        if request.headers.get("host").lower() != AP_DOMAIN.lower():
+            return render_template(
+                f"{AP_TEMPLATE_PATH}/redirect.html", domain=AP_DOMAIN.lower()
+            )
+
+        return render_template(f"{AP_TEMPLATE_PATH}/index.html")
+
+    def ap_configure(request):
+        print("Saving wifi credentials...")
+
+        with open(WIFI_FILE, "w") as f:
+            json.dump(request.form, f)
+            f.close()
+
+        # Reboot from new thread after we have responded to the user.
+        _thread.start_new_thread(machine_reset, ())
+        return render_template(
+            f"{AP_TEMPLATE_PATH}/configured.html", ssid=request.form["ssid"]
+        )
+
+    def ap_catch_all(request):
+        if request.headers.get("host") != AP_DOMAIN:
+            return render_template(
+                f"{AP_TEMPLATE_PATH}/redirect.html", domain=AP_DOMAIN
+            )
+
+        return "Not found.", 404
+
+    server.add_route("/", handler=ap_index, methods=["GET"])
+    server.add_route("/configure", handler=ap_configure, methods=["POST"])
+    server.set_callback(ap_catch_all)
+
+    ap = access_point(AP_NAME)
+    ip = ap.ifconfig()[0]
+    dns.run_catchall(ip)
+
+
+def application_mode():
+    print("Entering application mode.")
+    onboard_led = machine.Pin("LED", machine.Pin.OUT)
+
+    def app_left_a_bit(request):
+        def action(revs, mode, speed):
+            input(f"Press <Enter> to continue {revs=} {mode=} {speed=} ")
+            mot.revolutions(revs, mode, speed)
+
+            return abs(revs * mot.steps_per_revolution - mot.progress)
+
+        try:
+            # full step variants, different speeds and directions
+            # <number of steps>, <step type>, <step frequency>
+            action(1, "Full", 50)
+            action(-1, "Full", 100)
+            action(5, "Full", 400)
+            action(-5, "Full", 600)
+            # microstepping variants
+            action(1, "Half", 500)
+            action(-1, "1/4", 1000)
+            action(5, "1/8", 2000)
+            action(-5, "1/16", 4000)
+            action(3, "1/32", 8000)
+        except KeyboardInterrupt:
+            print("Interrupted from Keyboard")
+        mot.disable()
+
+    def app_index(request):
+        return render_template(f"{APP_TEMPLATE_PATH}/index.html")
+
+    def app_toggle_led(request):
+        onboard_led.toggle()
+        return "OK"
+
+    def app_get_temperature(request):
+        # Not particularly reliable but uses built in hardware.
+        # Demos how to incorporate senasor data into this application.
+        # The front end polls this route and displays the output.
+        # Replace code here with something else for a 'real' sensor.
+        # Algorithm used here is from:
+        # https://www.coderdojotc.org/micropython/advanced-labs/03-internal-temperature/
+        sensor_temp = machine.ADC(4)
+        reading = sensor_temp.read_u16() * (3.3 / (65535))
+        temperature = 27 - (reading - 0.706) / 0.001721
+        return f"{round(temperature, 1)}"
+
+    def app_reset(request):
+        # Deleting the WIFI configuration file will cause the device to reboot as
+        # the access point and request new configuration.
+        os.remove(WIFI_FILE)
+        # Reboot from new thread after we have responded to the user.
+        _thread.start_new_thread(machine_reset, ())
+        return render_template(
+            f"{APP_TEMPLATE_PATH}/reset.html", access_point_ssid=AP_NAME
+        )
+
+    def app_catch_all(request):
+        return "Not found.", 404
+
+    server.add_route("/", handler=app_index, methods=["GET"])
+    server.add_route("/leftabit", handler=app_left_a_bit, methods=["GET"])
+    server.add_route("/toggle", handler=app_toggle_led, methods=["GET"])
+    server.add_route("/temperature", handler=app_get_temperature, methods=["GET"])
+    server.add_route("/reset", handler=app_reset, methods=["GET"])
+    # Add other routes for your application...
+    server.set_callback(app_catch_all)
+
+
+# Figure out which mode to start up in...
 if (mot := drv8825_setup.setup_stepper()) is None:
     print("No stepper driver")
     sys.exit()
-enc = None
-print(mot)
+try:
+    os.stat(WIFI_FILE)
 
-run_test(mot, enc, button)
+    # File was found, attempt to connect to wifi...
+    with open(WIFI_FILE) as f:
+        wifi_current_attempt = 1
+        wifi_credentials = json.load(f)
 
-print("TEST END")
+        while wifi_current_attempt < WIFI_MAX_ATTEMPTS:
+            ip_address = connect_to_wifi(
+                wifi_credentials["ssid"], wifi_credentials["password"]
+            )
 
-# =====================END===========================
+            if is_connected_to_wifi():
+                print(f"Connected to wifi, IP address {ip_address}")
+                break
+            else:
+                wifi_current_attempt += 1
 
+        if is_connected_to_wifi():
+            application_mode()
+        else:
+            # Bad configuration, delete the credentials file, reboot
+            # into setup mode to get new credentials from the user.
+            print("Bad wifi connection!")
+            print(wifi_credentials)
+            os.remove(WIFI_FILE)
+            machine_reset()
+
+except Exception:
+    # Either no wifi configuration file found, or something went wrong,
+    # so go into setup mode.
+    setup_mode()
+
+# Start the web server...
+server.run()
